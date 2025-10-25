829223006990b0fd899ae3b5bc864941911d6786
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-25 15:30:23 +0600

    enable configuration server in ConfigServerServiceApplication

--- config-server-service/src/main/java/com/example/stego/config_server_service/ConfigServerServiceApplication.java
+++ config-server-service/src/main/java/com/example/stego/config_server_service/ConfigServerServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.stego.config_server_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerServiceApplication.class, args);
-	}
+    static void main(String[] args) {
+        SpringApplication.run(ConfigServerServiceApplication.class, args);
+    }
 
 }
