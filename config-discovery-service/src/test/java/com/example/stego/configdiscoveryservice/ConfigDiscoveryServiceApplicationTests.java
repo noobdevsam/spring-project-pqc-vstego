7983d245ff06ec3fb3d4a3fd8bbfaa16078b0c43
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-25 18:03:23 +0600

    initialize config discovery service with Maven setup and basic application structure

---
+++ config-discovery-service/src/test/java/com/example/stego/configdiscoveryservice/ConfigDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stego.configdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
