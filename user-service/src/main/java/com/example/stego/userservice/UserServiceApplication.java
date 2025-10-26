4bff80ba8ddfc37752506eb7690eab2c1c8a8f27
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-27 01:01:23 +0600

    feat: enable service discovery and MongoDB auditing in UserServiceApplication

--- user-service/src/main/java/com/example/stego/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/stego/userservice/UserServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.stego.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMongoAuditing // Enable auto population of auditing fields
 public class UserServiceApplication {
 
     static void main(String[] args) {
