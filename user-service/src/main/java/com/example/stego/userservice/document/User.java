f871f73847bae02cfc9baa9f190cb6fd824393e9
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 00:56:55 +0600

    feat: override equals, hashCode, and toString methods in User model

--- user-service/src/main/java/com/example/stego/userservice/document/User.java
+++ user-service/src/main/java/com/example/stego/userservice/document/User.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Document(collection = "users")
 public class User {
@@ -97,5 +98,39 @@ public class User {
 
     public void setKeyLastUpdatedAt(Instant keyLastUpdatedAt) {
         this.keyLastUpdatedAt = keyLastUpdatedAt;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof User user)) return false;
+
+        return Objects.equals(id, user.id) && Objects.equals(githubId, user.githubId) && Objects.equals(username, user.username) && Objects.equals(avatarUrl, user.avatarUrl) && Objects.equals(pqcPublicKey, user.pqcPublicKey) && Objects.equals(pqcPrivateKey, user.pqcPrivateKey) && Objects.equals(createdAt, user.createdAt) && Objects.equals(keyLastUpdatedAt, user.keyLastUpdatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(githubId);
+        result = 31 * result + Objects.hashCode(username);
+        result = 31 * result + Objects.hashCode(avatarUrl);
+        result = 31 * result + Objects.hashCode(pqcPublicKey);
+        result = 31 * result + Objects.hashCode(pqcPrivateKey);
+        result = 31 * result + Objects.hashCode(createdAt);
+        result = 31 * result + Objects.hashCode(keyLastUpdatedAt);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", githubId='" + githubId + '\'' +
+                ", username='" + username + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                ", pqcPublicKey='" + pqcPublicKey + '\'' +
+                ", pqcPrivateKey='" + pqcPrivateKey + '\'' +
+                ", createdAt=" + createdAt +
+                ", keyLastUpdatedAt=" + keyLastUpdatedAt +
+                '}';
     }
 }
