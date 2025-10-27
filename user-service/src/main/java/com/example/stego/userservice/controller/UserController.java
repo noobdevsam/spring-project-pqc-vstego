df1c689dfc3a7c7838146b208f5eea57500e81a2
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 00:37:36 +0600

    feat: add getCurrentUser and getAllUsers methods to UserController

--- user-service/src/main/java/com/example/stego/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/stego/userservice/controller/UserController.java
@@ -6,8 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -19,6 +22,7 @@ public class UserController {
         this.appUserService = appUserService;
     }
 
+    @GetMapping("/me")
     public ResponseEntity<UserDTO> getCurrentUser(
             @AuthenticationPrincipal OAuth2AuthenticationToken authenticationToken
     ) {
@@ -29,6 +33,11 @@ public class UserController {
 
         var userDTO = appUserService.findOrCreateUser(authenticationToken);
         return ResponseEntity.ok(userDTO);
+    }
+
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        var users = appUserService.findAllUsers();
+        return ResponseEntity.ok(users);
     }
 
 }
