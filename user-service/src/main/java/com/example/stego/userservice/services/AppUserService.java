995417b8578f6c09526199a2f6f42329c73a4130
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-27 23:50:58 +0600

    feat: add AppUserService interface for user management

---
+++ user-service/src/main/java/com/example/stego/userservice/services/AppUserService.java
@@ -0,0 +1,13 @@
+package com.example.stego.userservice.services;
+
+import com.example.stego.userservice.model.UserDTO;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+import java.util.List;
+
+public interface AppUserService {
+
+    UserDTO findOrCreateUser(OAuth2AuthenticationToken authenticationToken);
+
+    List<UserDTO> findAllUsers();
+}
