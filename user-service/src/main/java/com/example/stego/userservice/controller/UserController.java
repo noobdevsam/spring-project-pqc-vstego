3df0e95a6ce37e3b9a181d29e378852aa8e3110d
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 00:27:49 +0600

    feat: add AppUserService dependency to UserController

--- user-service/src/main/java/com/example/stego/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/stego/userservice/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.example.stego.userservice.controller;
 
+import com.example.stego.userservice.services.AppUserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+
+    private final AppUserService appUserService;
+
+    public UserController(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
 }
