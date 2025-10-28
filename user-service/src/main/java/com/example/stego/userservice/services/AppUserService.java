d942b82966fe468bbae42ba240cb43cec87e071c
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 09:53:57 +0600

    feat: add methods for updating PQC keys and retrieving private key in AppUserService

--- user-service/src/main/java/com/example/stego/userservice/services/AppUserService.java
+++ user-service/src/main/java/com/example/stego/userservice/services/AppUserService.java
@@ -1,5 +1,6 @@
 package com.example.stego.userservice.services;
 
+import com.example.stego.userservice.model.KeyUpdateRequest;
 import com.example.stego.userservice.model.UserDTO;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
@@ -10,4 +11,8 @@ public interface AppUserService {
     UserDTO findOrCreateUser(OAuth2AuthenticationToken authenticationToken);
 
     List<UserDTO> findAllUsers();
+
+    UserDTO updatePqcKeys(OAuth2AuthenticationToken authenticationToken, KeyUpdateRequest keyUpdateRequest);
+
+    String getPrivateKey(OAuth2AuthenticationToken authenticationToken);
 }
