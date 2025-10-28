f88a08d4405c1b558a668724114b25653bc3ee79
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 17:58:03 +0600

    feat: implement KafkaProducerServiceImpl for handling encode and decode requests

--- orchestration-service/src/main/java/com/example/stego/orchestrationservice/services/KafkaProducerService.java
+++ orchestration-service/src/main/java/com/example/stego/orchestrationservice/services/KafkaProducerService.java
@@ -1,11 +1,12 @@
 package com.example.stego.orchestrationservice.services;
 
+import com.example.stego.orchestrationservice.model.KafkaDecodeRequest;
 import com.example.stego.orchestrationservice.model.KafkaEncodeRequest;
 
 public interface KafkaProducerService {
 
     void sendEncodeRequest(KafkaEncodeRequest kafkaEncodeRequest);
 
-    void sendDecodeRequest(KafkaEncodeRequest kafkaEncodeRequest);
+    void sendDecodeRequest(KafkaDecodeRequest kafkaDecodeRequest);
 
 }
