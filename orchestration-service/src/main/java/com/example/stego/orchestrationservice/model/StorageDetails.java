0c1382c9401c5948a2a943be47a6b3a7efcc2b9c
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 17:34:29 +0600

    feat: add constructors to StorageDetails class using Lombok @NoArgsConstructor and @AllArgsConstructor annotations

--- orchestration-service/src/main/java/com/example/stego/orchestrationservice/model/StorageDetails.java
+++ orchestration-service/src/main/java/com/example/stego/orchestrationservice/model/StorageDetails.java
@@ -1,9 +1,13 @@
 package com.example.stego.orchestrationservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class StorageDetails {
     private String inputFileGridFsId;
