2e0eb8d1ab6120e33eaddb2188d3104405caf586
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 17:30:06 +0600

    feat: refactor StorageDetails class to use Lombok @Data and @EqualsAndHashCode annotations for cleaner code

--- orchestration-service/src/main/java/com/example/stego/orchestrationservice/model/StorageDetails.java
+++ orchestration-service/src/main/java/com/example/stego/orchestrationservice/model/StorageDetails.java
@@ -1,48 +1,12 @@
 package com.example.stego.orchestrationservice.model;
 
-import java.util.Objects;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@EqualsAndHashCode
 public class StorageDetails {
     private String inputFileGridFsId;
     private String secretFileGridFsId; // Only for ENCODE jobs
     private String outputFileGridFsId;// For COMPLETED jobs
-
-    public String getSecretFileGridFsId() {
-        return secretFileGridFsId;
-    }
-
-    public void setSecretFileGridFsId(String secretFileGridFsId) {
-        this.secretFileGridFsId = secretFileGridFsId;
-    }
-
-    public String getInputFileGridFsId() {
-        return inputFileGridFsId;
-    }
-
-    public void setInputFileGridFsId(String inputFileGridFsId) {
-        this.inputFileGridFsId = inputFileGridFsId;
-    }
-
-    public String getOutputFileGridFsId() {
-        return outputFileGridFsId;
-    }
-
-    public void setOutputFileGridFsId(String outputFileGridFsId) {
-        this.outputFileGridFsId = outputFileGridFsId;
-    }
-
-    @Override
-    public final boolean equals(Object o) {
-        if (!(o instanceof StorageDetails that)) return false;
-
-        return Objects.equals(inputFileGridFsId, that.inputFileGridFsId) && Objects.equals(secretFileGridFsId, that.secretFileGridFsId) && Objects.equals(outputFileGridFsId, that.outputFileGridFsId);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hashCode(inputFileGridFsId);
-        result = 31 * result + Objects.hashCode(secretFileGridFsId);
-        result = 31 * result + Objects.hashCode(outputFileGridFsId);
-        return result;
-    }
 }
