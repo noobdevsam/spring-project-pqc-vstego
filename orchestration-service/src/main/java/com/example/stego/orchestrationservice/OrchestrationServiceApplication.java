07edaccc372da3796a3f4a8fe5b84b26a0013975
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 13:49:22 +0600

    feat: enable service discovery, MongoDB auditing, and Kafka support

--- orchestration-service/src/main/java/com/example/stego/orchestrationservice/OrchestrationServiceApplication.java
+++ orchestration-service/src/main/java/com/example/stego/orchestrationservice/OrchestrationServiceApplication.java
@@ -2,8 +2,14 @@ package com.example.stego.orchestrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableMongoAuditing
+@EnableKafka
 public class OrchestrationServiceApplication {
 
     static void main(String[] args) {
