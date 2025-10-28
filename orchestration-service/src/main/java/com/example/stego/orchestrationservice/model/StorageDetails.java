da95368e6b64dd32e3204318d6d0e125aad0a0a1
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-28 17:40:00 +0600

    feat: add KafkaJobCompletion class with Lombok annotations for job completion handling

--- orchestration-service/src/main/java/com/example/stego/orchestrationservice/model/StorageDetails.java
+++ orchestration-service/src/main/java/com/example/stego/orchestrationservice/model/StorageDetails.java
@@ -2,13 +2,11 @@ package com.example.stego.orchestrationservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class StorageDetails {
     private String inputFileGridFsId;
     private String secretFileGridFsId; // Only for ENCODE jobs
