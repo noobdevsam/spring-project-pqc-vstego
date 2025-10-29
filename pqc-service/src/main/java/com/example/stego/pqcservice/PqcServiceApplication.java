e4bfc19cfe8ace8f2296a5d0f97aafe4fb82dba8
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-29 11:38:24 +0600

    feat: initialize pqc-service with Maven setup and basic application structure

---
+++ pqc-service/src/main/java/com/example/stego/pqcservice/PqcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stego.pqcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqcServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(PqcServiceApplication.class, args);
+    }
+
+}
