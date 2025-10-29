ea2440eb13c868e5b38d914f9ff4cbcaa2b5ed9c
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-29 12:55:19 +0600

    feat: implement key pair generation using Kyber and Dilithium algorithms in PqcServiceImpl

--- pqc-service/src/main/java/com/example/stego/pqcservice/sevices/impl/PqcServiceImpl.java
+++ pqc-service/src/main/java/com/example/stego/pqcservice/sevices/impl/PqcServiceImpl.java
@@ -7,8 +7,11 @@ import com.example.stego.pqcservice.repo.PublicKeyRepo;
 import com.example.stego.pqcservice.sevices.PqcService;
 import lombok.RequiredArgsConstructor;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.pqc.jcajce.spec.DilithiumParameterSpec;
+import org.bouncycastle.pqc.jcajce.spec.KyberParameterSpec;
 import org.springframework.stereotype.Service;
 
+import java.security.KeyPairGenerator;
 import java.security.Security;
 import java.util.Optional;
 
@@ -26,8 +29,17 @@ public class PqcServiceImpl implements PqcService {
     private final PublicKeyRepo publicKeyRepo;
 
     @Override
-    public KeyPairDTO generateKeys() {
-        return null;
+    public KeyPairDTO generateKeys() throws Exception {
+        // Generate a key pair using a PQC algorithm
+        var kemGenerator = KeyPairGenerator.getInstance("Kyber", BouncyCastleProvider.PROVIDER_NAME);
+        kemGenerator.initialize(KyberParameterSpec.kyber1024);
+        var kemKeyPair = kemGenerator.generateKeyPair();
+
+        // Generate Dilithium key pair
+        var dsaGenerator = KeyPairGenerator.getInstance("Dilithium", BouncyCastleProvider.PROVIDER_NAME);
+        dsaGenerator.initialize(DilithiumParameterSpec.dilithium5);
+        var dsaKeyPair = dsaGenerator.generateKeyPair();
+
     }
 
     @Override
