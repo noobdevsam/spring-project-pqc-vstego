03c3f1cb3c46662161012bd60b276e887c50cfc5
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-30 20:35:48 +0600

    feat: initialize file service application with Maven setup and basic configuration

---
+++ file-service/src/main/java/com/example/stego/fileservice/FileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stego.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FileServiceApplication.class, args);
+    }
+
+}
