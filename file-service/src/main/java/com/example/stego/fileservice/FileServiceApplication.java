a79b96a1756c37a3126a9c590ca6239fa72c3601
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-30 20:46:23 +0600

    feat: enable service discovery and scheduling in FileServiceApplication

--- file-service/src/main/java/com/example/stego/fileservice/FileServiceApplication.java
+++ file-service/src/main/java/com/example/stego/fileservice/FileServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.stego.fileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 public class FileServiceApplication {
 
     static void main(String[] args) {
