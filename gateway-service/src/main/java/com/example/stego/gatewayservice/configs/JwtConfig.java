ec75e289ffecb7319a85c0053d656d4bfdeb1419
noobdevsam <mdshoykotislam05@gmail.com>
2025-10-31 02:59:09 +0600

    feat: add JwtEncoder bean for JWT encoding using NimbusJwtEncoder

--- gateway-service/src/main/java/com/example/stego/gatewayservice/configs/JwtConfig.java
+++ gateway-service/src/main/java/com/example/stego/gatewayservice/configs/JwtConfig.java
@@ -6,6 +6,8 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -48,4 +50,10 @@ public class JwtConfig {
         var jwkSet = new JWKSet(rsaKey);
         return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
     }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
 }
