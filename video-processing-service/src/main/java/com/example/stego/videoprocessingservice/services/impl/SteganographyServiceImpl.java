de061ab51728d7e145da1ab2b33a2b1ad404ad2d
noobdevsam <mdshoykotislam05@gmail.com>
2025-11-01 12:18:17 +0600

    feat: implement SteganographyService with methods for payload embedding and extraction

---
+++ video-processing-service/src/main/java/com/example/stego/videoprocessingservice/services/impl/SteganographyServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.stego.videoprocessingservice.services.impl;
+
+import com.example.stego.videoprocessingservice.services.SteganographyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SteganographyServiceImpl implements SteganographyService {
+
+    @Override
+    public void embedPayload(InputStream carrierVideoStream, byte[] payloadData, OutputStream stegoVideoOutputStream) throws IOException, InterruptedException {
+
+    }
+
+    @Override
+    public byte[] extractPayload(InputStream stegoVideoInputStream) throws IOException, InterruptedException {
+        return new byte[0];
+    }
+
+    @Override
+    public Map<String, Object> getVideoInfo(InputStream videoStream) throws IOException, InterruptedException {
+        return Map.of();
+    }
+
+    @Override
+    public String uploadFile(InputStream fileStream, String fileName, String contentType, String ownerId) {
+        return "";
+    }
+
+}
