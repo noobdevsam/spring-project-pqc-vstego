67fb08e4cbc25fed2afa7a00d07c786db3c2c39a
noobdevsam <mdshoykotislam05@gmail.com>
2025-11-01 11:04:29 +0600

    feat: initialize video processing service with Maven setup and basic application structure

---
+++ video-processing-service/src/main/java/com/example/stego/videoprocessingservice/VideoProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stego.videoprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoProcessingServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VideoProcessingServiceApplication.class, args);
+    }
+
+}
