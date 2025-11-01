4e8b3d35248a94d25b615cf00ff5257dae049fa6
noobdevsam <mdshoykotislam05@gmail.com>
2025-11-01 11:20:28 +0600

    feat: enable service discovery and Kafka support in video processing application

--- video-processing-service/src/main/java/com/example/stego/videoprocessingservice/VideoProcessingServiceApplication.java
+++ video-processing-service/src/main/java/com/example/stego/videoprocessingservice/VideoProcessingServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.stego.videoprocessingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableKafka
 public class VideoProcessingServiceApplication {
 
     static void main(String[] args) {
